9720b478b653b31fdccc4975dcf47dc52540d357
Gary Powell-Jones <g.powell-jones@sharepointresource.co.uk>
2018-06-19 12:25:46 +0100

    Added service reference Moved helpers and models to separate files

---
+++ SPR.Provisioning/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Extensibility;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers;
+using OfficeDevPnP.Core.Framework.Provisioning.Connectors;
+using System.IO;
+
+namespace SPR.Provisioning
+{
+    class Helpers
+    {
+        private const string workflowFolderName = "NintexWorkflows";
+
+        public void DownloadWorkflowFile(string workflowID, FileConnectorBase writer, Stream fileStream, WorkflowLevel workflowLevel)
+        {
+            writer.SaveFileStream(workflowID, workflowFolderName, fileStream);
+        }
+    }
+}
